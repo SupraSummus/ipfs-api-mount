31cdcb4d29ec09396aba486227f13d832b3d0b39
Jan Rydzewski <flegmer@gmail.com>
2018-05-30 19:18:18 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python
 from setuptools import setup
 from distutils.command.build import build
+from setuptools.command.develop import develop
 from subprocess import check_call
+
+
+def compile_protobuf():
+    check_call(['protoc', '--python_out=.', 'ipfs_api_mount/unixfs.proto'])
 
 
 class custom_build(build):
     def run(self):
         if not self.dry_run:
-            check_call(['protoc', '--python_out=.', 'ipfs_api_mount/unixfs.proto'])
+            compile_protobuf()
         build.run(self)
+
+
+class custom_develop(develop):
+    def run(self):
+        if not self.dry_run:
+            compile_protobuf()
+        develop.run(self)
 
 
 setup(
     name='ipfs_api_mount',
-    version='1.0',
+    version='0.1.0',
     description='Mount IPFS directory as local FS.',
+    license='MIT',
+    url='https://github.com/SupraSummus/ipfs-api-mount',
+    classifiers=[
+        'Topic :: System :: Filesystems',
+    ],
+    keywords='ipfs fuse mount fs',
     install_requires=[
         'fusepy',
         'ipfsapi',
@@ -25,5 +43,8 @@ setup(
     ],
     packages=['ipfs_api_mount'],
     scripts=['bin/ipfs-api-mount'],
-    cmdclass={'build': custom_build},
+    cmdclass={
+        'build': custom_build,
+        'develop': custom_develop,
+    },
 )
